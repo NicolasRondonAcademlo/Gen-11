6178e8800ade6c69b0205fa6133ff7d4a4c84301
Nicolas <nicolas.rondon@crehana.com>
2022-06-29 19:43:46 -0500

    Update oop.py

--- 29-06-22/oop.py
+++ 29-06-22/oop.py
@@ -11,6 +11,8 @@ class Person():
     def __init__(self, name, age, telf=None) -> None:
         self.name = name
         self.age = age
+        self.telf = telf
+        print(self.saluda())
 
     def saluda(self): 
         return f"Hola {self.name}"
@@ -19,13 +21,22 @@ class Person():
 
 
     
-roberto = Person("Roberto", 85)
-print(roberto)
-print(roberto.name)
-roberto.telefono = 55488
-print(roberto.telefono)
-print(roberto.saluda())
-alberto = Person("Alberto", 85)
-roberto.address = "22222"
-alber_2 =  Person("Alberto", 85)
+roberto = Person("Roberto", 85, 8897779)
 
+# Empresa 
+# Empleado
+# Trabajo
+# Vehiculo
+
+class Empresa:
+    country = "Colombia"
+
+    def __init__(self, name:str, addres:str) -> None:
+        self.name = name
+        self.addres = addres
+
+    def do_contract(self, employe):
+        print(f"Se contracto a {employe} en la empresa {self.name}")
+
+academlo = Empresa("academlo", "calle muy lejos")
+academlo.do_contract("Luis")
