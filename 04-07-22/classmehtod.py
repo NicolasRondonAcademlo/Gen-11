498ba72537283c69bf8e4537f76c5de4428bc664
Nicolas <nicolas.rondon@crehana.com>
2022-07-04 20:54:05 -0500

    a

---
+++ 04-07-22/classmehtod.py
@@ -0,0 +1,21 @@
+from datetime import date
+from mimetypes import init
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def calculate_age(cls, name, birth_year):
+        return f"La edad de {name} es {date.today().year - birth_year} "
+
+    def show(self):
+        return f"La edad de {self.name} es {self.age}"
+
+
+jessa = Student("Jessa", 20)
+print(jessa.show())
+
+joy = Student.calculate_age("Joy", 1995)
+print(joy)
