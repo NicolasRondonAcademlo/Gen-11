5c0bc3dc5408568eb10c8df38089b54ad225ea1a
Nicolas <nicolas.rondon@crehana.com>
2022-07-18 20:47:09 -0500

    Update views.py

--- 18-07-22/my_app/views.py
+++ 18-07-22/my_app/views.py
@@ -1,27 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 import datetime
 from .models import Food
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def hello_world(reqest):
     hello = "Hola Gen 11"
     html = f"<html><h1>{hello}</h1></html>"
     return HttpResponse(html)
 
+@csrf_exempt
 def all_fruits(request):
-    food = Food.objects.all()
-    print(food)
-    names = food.values()
-    #names = food.values_list("name", flat=True)
-    print(names)
-    result_list_name = []
-    for name in names:
-        text = f"<li>{name['name'] }-{name['quantity']}-{name['description']}</li>"
-        result_list_name.append(text)
+    if request.method == "GET":
+        food = Food.objects.all()
+        names = food.values()
+        #names = food.values_list("name", flat=True)
+        result_list_name = []
+        for name in names:
+            text = f"<li>{name['name'] }-{name['quantity']}-{name['description']}</li>"
+            result_list_name.append(text)
 
-    result_list_string = ""
-    for i in result_list_name:
-        result_list_string += i
-    result_list = f"<ul>{result_list_string}</ul>"
-    return HttpResponse(result_list)
+        result_list_string = ""
+        for i in result_list_name:
+            result_list_string += i
+        result_list = f"<ul>{result_list_string}</ul>"
+        return HttpResponse(result_list)
+    if request.method == "POST":
+        data = json.loads(request.body)
+        nueva = Food(
+            name=data.get("name"),
+            description=data["description"],
+            quantity=data["quantity"]
+        )
+        nueva.save()
+        last_fruit = Food.objects.all().last()
+        return JsonResponse({
+            "name": last_fruit.name,
+            "uuid": last_fruit.uuid
+        })
+
+# Vamos a crear un modelo de mascotas y vamos a renderizar en el navegador
+# nombre, edad, animal
+
+# Vamos a crear un modelo de comida preparada  y vamos a renderizar
+# Los ingredientes y tiempo de coccion
